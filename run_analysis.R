f9951234e526048ca6c0cac9f090c50b0d57e5bf
mfrank99 <michael.frank@gatech.edu>
2015-11-22 18:32:02 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,10 +3,13 @@ library(dplyr)
 
 #Get header, Activity Labels
 data_header <- read.table("features.txt",col.names = c("id","variable"))
+data_header$variable <- gsub(',','_',data_header$variable)
+data_header$variable <- gsub("\\()","",data_header$variable)
+data_header
 data_activities <- read.table("activity_labels.txt",col.names = c("id","activity"))
 
 #Get Training Data
-train_data <- read.table("train/X_train.txt",col.names=data_header$variable)
+train_data <- read.table("train/X_train.txt",col.names=data_header$variable)#,check.names = FALSE)
 #Get Subject data
 train_subjects <- read.table("train/subject_train.txt",col.names = "subject")
 #Get Activity Data
@@ -20,7 +23,7 @@ remove(train_data,train_activity,train_subjects)
 
 #Get Test, Subject, Activity Data
 #Get testing Data
-test_data <- read.table("test/X_test.txt",col.names=data_header$variable)
+test_data <- read.table("test/X_test.txt",col.names=data_header$variable)#,check.names = FALSE)
 #Get Subject data
 test_subjects <- read.table("test/subject_test.txt",col.names = "subject")
 #Get Activity Data
@@ -36,11 +39,11 @@ remove(test_data,test_activity,test_subjects)
 my_data <- rbind(my_data1,my_data2)
 remove(my_data1,my_data2)
 
-#Get Avereages by Activity and Subject
+#Get Averages by Activity and Subject
 my_summary <- my_data %>% group_by(subject,activity) %>% summarise_each(funs(mean)) #%>% arrange(activity,subject)
 my_summary <- my_summary %>% arrange(activity,subject)
 data.frame(my_summary)
 
-#Output final data to txt files
 write.table(my_data,"tidy_data.txt",sep = ",",row.names = FALSE)
 write.table(my_summary,"tidy_summary.txt",sep = ",",row.names = FALSE)
+
